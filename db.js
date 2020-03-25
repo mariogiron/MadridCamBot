109d8ce9bf88177545fe3ea6a23cd3ec00a9e840
Mario Giron Martin <mariogironm@gmail.com>
2020-03-25 23:44:24 +0100

    Database

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 const UserModel = require('./models/user')
 const CameraModel = require('./models/camera')
 
-const sequelize = new Sequelize('mysql://b0582d22f049d0:adbca5c4@eu-cdbr-west-02.cleardb.net/heroku_3ca53332a861b27');
+const sequelize = new Sequelize(process.env.DATABASE_MYSQL);
 
 const User = UserModel(sequelize, Sequelize)
 const Camera = CameraModel(sequelize, Sequelize);
